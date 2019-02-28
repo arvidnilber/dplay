d07aa99ec319deefbb929153613aa4ae75887d31
Arvid Nilber <aanilber@gmail.com>
2019-02-28 09:00:05 +0100

    hejasd

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ class Loading extends Component {
     if(this.props.home){
         return (
             <div className="wrapper">
-            <a href="https://dplay.se" target="_blank">KLICKA HÄR OM DET LADDAR FÖR EVIGT OCH LADDA SEDAN OM DENNA SIDA</a>
+            <a href="https://dplay.se" rel="noopener noreferrer" target="_blank">KLICKA HÄR OM DET LADDAR FÖR EVIGT OCH LADDA SEDAN OM DENNA SIDA</a>
             <div className="loading">
                 <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
                     <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
